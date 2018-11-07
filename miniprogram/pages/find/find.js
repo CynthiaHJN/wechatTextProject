e92de6daa09c8a8e8857840ba4e163dbf3b92fa1
HeJiaNi <17816869761@163.com>
2018-11-07 18:07:36 +0800

    find

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -4,62 +4,67 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    findList: [
+      {
+        picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
+        title: '时令新鲜水果脐时令新鲜水果脐橙5斤 超甜',
+        price: '45.00',
+        commentNum: '100',
+        goodPercent: '96'
+      }, {
+        picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
+        title: '时令新鲜水果脐橙5斤 超甜',
+        price: '45.00',
+        commentNum: '100',
+        goodPercent: '96'
+      }, {
+        picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
+        title: '时令新鲜水果脐橙5斤 超甜',
+        price: '45.00',
+        commentNum: '100',
+        goodPercent: '96'
+      }, {
+        picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
+        title: '时令新鲜水果脐橙5斤 超甜',
+        price: '45.00',
+        commentNum: '100',
+        goodPercent: '96'
+      }, {
+        picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
+        title: '时令新鲜水果脐橙5斤 超甜',
+        price: '45.00',
+        commentNum: '100',
+        goodPercent: '96'
+      }, {
+        picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
+        title: '时令新鲜水果脐橙5斤 超甜',
+        price: '45.00',
+        commentNum: '100',
+        goodPercent: '96'
+      },
+    ],
+    topBar: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  goToSearch: function () {
+    console.log('....');
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  onPageScroll: function(e) {
+    if(e.scrollTop>20) {
+      this.setData({
+        topBar: true
+      });
+    } else {
+      this.setData({
+        topBar: false
+      });
+    }
   }
 })
