d27a9170476f93e631088cc73d2d09afeb4ad506
HeJiaNi <17816869761@163.com>
2018-11-08 15:23:57 +0800

    find

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -10,37 +10,43 @@ Page({
         title: '时令新鲜水果脐时令新鲜水果脐橙5斤 超甜',
         price: '45.00',
         commentNum: '100',
-        goodPercent: '96'
+        goodPercent: '96',
+        id: 1
       }, {
         picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
         title: '时令新鲜水果脐橙5斤 超甜',
         price: '45.00',
         commentNum: '100',
-        goodPercent: '96'
+        goodPercent: '96',
+        id: 2
       }, {
         picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
         title: '时令新鲜水果脐橙5斤 超甜',
         price: '45.00',
         commentNum: '100',
-        goodPercent: '96'
+        goodPercent: '96',
+        id: 3
       }, {
         picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
         title: '时令新鲜水果脐橙5斤 超甜',
         price: '45.00',
         commentNum: '100',
-        goodPercent: '96'
+        goodPercent: '96',
+        id: 4
       }, {
         picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
         title: '时令新鲜水果脐橙5斤 超甜',
         price: '45.00',
         commentNum: '100',
-        goodPercent: '96'
+        goodPercent: '96',
+        id: 5
       }, {
         picUrl: 'https://6e69-nini-store-a15a86-1257989489.tcb.qcloud.la/goods-image/goods-image-1.jpg?sign=e3f8790be8d9a8a557767bd1dec06a7c&t=1541578473',
         title: '时令新鲜水果脐橙5斤 超甜',
         price: '45.00',
         commentNum: '100',
-        goodPercent: '96'
+        goodPercent: '96',
+        id: 6
       },
     ],
     topBar: false
@@ -54,6 +60,14 @@ Page({
 
   goToSearch: function () {
     console.log('....');
+  },
+
+  geToDetail: function(e) {
+    // let _this = this;
+    // console.log();
+    wx.navigateTo({
+      url: './goods-detail/goods-detail?id=' + e.currentTarget.dataset.id,
+    })
   },
 
   onPageScroll: function(e) {
